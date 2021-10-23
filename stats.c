f8e494d505525196f66f9f3486b3897734366671
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2021-10-23 12:08:18 +0530

    Update stats.c
    Computing min, max, average

--- stats.c
+++ stats.c
@@ -1,10 +1,24 @@
 #include "stats.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    struct Stats statistics;
+    float min, max, sum;
+    int i;
+    sum = max = min = 0;
+    //Compute min, max and average
+    for (i=0; i++; i<setlength)
+    {
+        sum = sum + numberset[i];
+        if(numberset[i] > max) max = numberset[i];
+        if(numberset[i] < min) min = numberset[i];
+    }
+    
+    //Update the struct members   
+    statistics.average = total/setlength;
+    statistics.min = min;
+    statistics.max = max;
+    
+    return statistics;
 }
 
 int emailAlertCallCount = 0;
