56b3beeb7d89b65a56412456468c767644f31016
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2021-10-23 11:32:53 +0530

    Create Alert.c
    Created an Alert functionality to update the alert count for both email and led

---
+++ Alert.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "Alert.h"
+
+int emailAlertCallCount=0;
+int ledAlertCallCount=0;
+
+//When the LED alert is raised, the led alert counter is incremented
+void alert_by_led(){
+	ledAlertCallCount++;
+}
+//When the email alert is raised, the email alert counter is incremented
+void alert_by_email(){
+	emailAlertCallCount++;
+}
